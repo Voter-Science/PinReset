0beb524f32456fff797d4cfe9e228080fb508999
Voter-Science <Info@Voter-Science.com>
2019-08-17 21:54:51 -0700

    misc

--- src/index.tsx
+++ src/index.tsx
@@ -1,86 +1,112 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-import * as XC from 'trc-httpshim/xclient'
-import * as common from 'trc-httpshim/common'
-import * as core from 'trc-core/core'
 import * as trcSheet from 'trc-sheet/sheet'
 
 import { SheetContainer, IMajorState } from 'trc-react/dist/SheetContainer'
 
 import * as bcl from 'trc-analyze/collections'
-import { PluginLink } from "trc-react/dist/PluginLink";
-import { ColumnNames } from "trc-sheet/sheetContents";
 
-import { PluginShell} from 'trc-react/dist/PluginShell';
-import { ColumnSelector} from 'trc-react/dist/ColumnSelector';
-import { ColumnCheck} from 'trc-react/dist/ColumnCheck';
+import { PluginShell } from 'trc-react/dist/PluginShell';
+import { ColumnSelector } from 'trc-react/dist/ColumnSelector';
+import { ColumnCheck } from 'trc-react/dist/ColumnCheck';
 
 declare var _trcGlobal: IMajorState;
 
 
 // Lets somebody lookup a voter, and then answer questions about them. 
 // See all answers in Audit. 
-export class App extends React.Component<{}, {    
-    _ci : trcSheet.IColumnInfo // currently selected column
-    _mapping : any, // maps from _Ci's possible values to a color code. 
+export class App extends React.Component<{}, {
+    _ci: trcSheet.IColumnInfo // currently selected column
+    _mapping: any, // maps from _Ci's possible values to a color code. 
 }>
 {
     public constructor(props: any) {
         super(props);
 
-        this.state = {            
-            _ci : undefined,
-            _mapping : { }
+        this.state = {
+            _ci: undefined,
+            _mapping: {}
         };
         this.renderBody1 = this.renderBody1.bind(this);
         this.columnUpdate = this.columnUpdate.bind(this);
         this.onColorChanged = this.onColorChanged.bind(this);
         this.onApply = this.onApply.bind(this);
-        
+        this.onRemoveColor = this.onRemoveColor.bind(this);
     }
 
-    private columnUpdate(ci : trcSheet.IColumnInfo) {
+    private columnUpdate(ci: trcSheet.IColumnInfo) {
         // This will cause a re-render of renderColorChoices
         this.setState({
-            _ci : ci
+            _ci: ci
         });
     }
 
+    // generate a TRC expression to handle the color. USe this for the new XColor column. 
+    private getColorExpression() {
+        var expr = "switch(" + this.state._ci.Name;
+        for (var name in this.state._mapping) {
+            var colorVal = this.state._mapping[name];
+            if (colorVal.length > 0) {
+                expr += ",'" + name + "','" + colorVal + "'";
+            }
+        }
+        expr += ")";
+
+        return expr;
+    }
+
     // Called after user has determined a color mapping and wants to apply it. 
-    private onApply()  {
-        alert(JSON.stringify(this.state._mapping));
+    private onApply() {
+        var expr = this.getColorExpression();
+
+        // SheetOps will pause the UI and handle errors. 
+        _trcGlobal.SheetOps.beginAdminOp((admin: trcSheet.SheetAdminClient) => {
+            return admin.postNewExpressionAsync("XColor", expr);
+        });
     }
 
-    static colors : any = {
-        'Green' : 'g',
-        'Red' : 'r',
-        'Blue' : 'b',
-        'Yellow' : 'y'        
+    // Remove the XColor column. 
+    private onRemoveColor(): void {
+        var ok = confirm("Are you sure you want to remove the custom pin coloring from this sheet? (this will delete the XColor column)");
+        if (!ok)
+        {
+            return;
+        }
+        _trcGlobal.SheetOps.beginAdminOp(admin => {
+            return admin.postOpDeleteQuestionAsync("XColor");
+        });
     }
 
-    private onColorChanged(event : any, columnValue : string) {
+    private onColorChanged(event: any, columnValue: string) {
         var idx = event.target.value;
 
         var map = this.state._mapping;
-        map[columnValue]  = idx;
-        this.setState({_mapping : map});
+        map[columnValue] = idx;
+        this.setState({ _mapping: map });
     }
-    private renderColorChoices(columnValue : string) {
-        return <select onChange={x => this.onColorChanged(x,columnValue)} >            
+    private renderColorChoices(columnValue: string) {
+        return <select onChange={x => this.onColorChanged(x, columnValue)} >
             <option key="k1" value="">(none)</option>
+            <option key="k4" value="r">Red</option>
             <option key="k2" value="g">Green</option>
             <option key="k3" value="b">Blue</option>
-            <option key="k4" value="r">Red</option>
+            <option key="k2" value="p">Purple</option>
+            <option key="k2" value="o">Orange</option>
+            <option key="k2" value="y">Yellow</option>
         </select>
     }
 
     // Given a Column, let us select a color for each possible value. 
     private renderValues() {
         var ci = this.state._ci;
-        if (!ci || !ci.PossibleValues || ci.PossibleValues.length==0) {
+        // if (!ci || !ci.PossibleValues || ci.PossibleValues.length == 0) {
+        if (!ci) {
             return <div>(select a column)</div>
         }
+
+        var vals = ci.PossibleValues;
+
         return <table>
             <thead>
                 <tr>
@@ -88,7 +114,8 @@ export class App extends React.Component<{}, {
                     <td>Color</td>
                 </tr>
             </thead>
-            {ci.PossibleValues.map((columnValue,idx) =>                
+            <tbody>
+                {ci.PossibleValues.map((columnValue, idx) =>
                     <tr key={idx}>
                         <td>
                             {columnValue}
@@ -98,29 +125,35 @@ export class App extends React.Component<{}, {
                         </td>
                     </tr>
                 )}
+            </tbody>
         </table>
     }
 
-    private renderCurrentColor(ci : trcSheet.IColumnInfo) { 
-        if (ci.Expression) {
-            return <div>{ci.Expression}</div>
-        }
-        return <div>(sheet has an existing color scheme)</div>
+    // Show the current coloring scheme.
+    // Caller has already validated it has one. 
+    private renderCurrentColor(ci: trcSheet.IColumnInfo) {
+        return  <div>
+            { ci.Expression ? 
+                <div>Current custom color scheme is: {ci.Expression}</div>  : 
+                <div>(sheet has an existing color scheme)</div>        
+            }
+            <button onClick={this.onRemoveColor}>Remove custom coloring!</button>
+        </div>        
     }
 
     private renderBody1() {
         {
             // <ListColumns Include={ci => ci.IsReadOnly && ci.PossibleValues != null}></ListColumns>
-            return <div>                
+            return <div>
                 <ColumnCheck columnName="XColor" OnFound={this.renderCurrentColor}>
                     Sheet does not have a custom color scheme
                 </ColumnCheck>
                 <div>------</div>
                 <div>Set color scheme based on a column's values:</div>
-                <ColumnSelector Include={ci=>true} OnChange={this.columnUpdate} ></ColumnSelector>      
-                {this.renderValues()}          
+                <ColumnSelector Include={ci => true} OnChange={this.columnUpdate} ></ColumnSelector>
+                {this.renderValues()}
                 <button onClick={this.onApply}>Apply!</button>
-             </div>
+            </div>
         }
     }
 
